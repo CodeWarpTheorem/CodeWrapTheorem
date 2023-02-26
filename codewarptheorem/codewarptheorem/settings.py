0fefc765d18e8d11f5ee81128a4e245994187dbc
CodeWarpTheorem <codewarptheorem@gmail.com>
2023-02-26 18:08:23 +0530

    aws db settings changed.

--- codewarptheorem/codewarptheorem/settings.py
+++ codewarptheorem/codewarptheorem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'codewarptheorem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'HOST':'cwtdevdb.cpx8l3ltttr7.us-east-1.rds.amazonaws.com',
+        'PORT':'5432',
+        'PASSWORD':'CodeWarpTheorem'
     }
 }
 
